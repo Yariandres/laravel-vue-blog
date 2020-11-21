fb83250bd55167855e5ecd0765eb8f79ed42ac4f
Yari Andres <yari.andres@gmail.com>
2020-11-21 16:26:52 +0100

    todo laravel + vue completed

--- todo-list/resources/js/vue/app.vue
+++ todo-list/resources/js/vue/app.vue
@@ -1,13 +1,67 @@
 <template>
-    <div class="">
-        Hello
+    <div class="todoListConteiner">
+        <div class="heading">
+            <h2 id="title">Todo List</h2>
+
+            <add-item-form
+                v-on:reloadlist="getList()"
+            />
+
+        </div>
+
+        <list-view
+            :items="items"
+            v-on:reloadlist="getList()"
+        />
+
     </div>
 </template>
 
 <script>
+import addItemForm from "./addItemForm";
+import listView from "./listView";
 
     export default {
-        data: "Hello World"
+        components: {
+            addItemForm,
+            listView
+        },
+
+        data: function() {
+            return {
+                items: []
+            }
+        },
+
+        methods: {
+            getList () {
+                axios.get('api/items')
+                .then( response => {
+                    this.items = response.data
+                })
+                .catch( error => console.log(error))
+            }
+        },
+
+        created() {
+            this.getList();
+        }
+    }
+</script>
+
+<style scoped>
+    .todoListConteiner {
+        width: 350px;
+        margin: auto;
     }
 
-</script>
+    .heading {
+        background: #eee;
+        margin: auto;
+    }
+
+    #title {
+        text-align: center;
+    }
+
+</style>
